c7b4518c70f1564d6a7a19922036dc9591ecfa2d
Andriy Shevchuk <a.shevchuk@zakaz.ua>
2021-06-17 10:33:35 +0300

    Rirname fixed.

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import isIndex from './helpers/isIndex';
 import whileRegex from './helpers/whileRegex';
 import getFixedPath from './helpers/getFixedPath';
 import * as fs from 'fs';
+import * as path from 'path';
 
 const dirPath = process.argv[2];
 console.log('dirPath: =============================', dirPath);
@@ -13,14 +14,15 @@ const fileList = allFiles.filter((path) => !hasIndex([path]));
 
 for (let i = 0; i < fileList.length; i++) {
   const filePath = fileList[i];
+  const dirName = path.dirname(filePath);
 
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
   let fileContentFixed: string = fileContent;
 
-  const importRegExp = /from ['"](?<path>.*)['"]/g;
+  const importRegExp = /from ['"]([\.\/].*)['"]/g;
 
   whileRegex(importRegExp, fileContent, (path) => {
-    const truePath = dirPath + '/' + path;
+    const truePath = dirName + '/' + path;
 
     if (isIndex(truePath)) {
       fileContentFixed = fileContentFixed.replace(path, getFixedPath(path));
